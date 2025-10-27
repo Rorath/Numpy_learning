89c889f0aeb37f97f1949011d7d0f4c3ffd48cb3
Rorath <lodave801@gmail.com>
2025-10-27 18:43:15 +0800

    2linear_algerbra.py updated

---
+++ 2linear_algerbra.py
@@ -0,0 +1,13 @@
+#matrix multiplication
+import numpy as np
+a = np.full((2,2), 9)
+b = np.full((2,3), 3)
+i = np.identity(2)
+print(np.matmul(a,b))
+
+#get the determinant of matrix
+c = np.random.randint(1,10,(3,3))
+det_c = np.linalg.det(c)
+print(det_c)
+
+#.........
